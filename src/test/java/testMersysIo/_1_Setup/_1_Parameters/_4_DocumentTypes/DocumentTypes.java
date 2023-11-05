ba8430531e95b2514f1b98ec34ac6d1516ff5f55
nesterch <terlemez.neslihan@gmail.com>
2023-11-05 12:00:15 +0100

    document types user story kodu tamamlandi

--- src/test/java/testMersysIo/_1_Setup/_1_Parameters/_4_DocumentTypes/DocumentTypes.java
+++ src/test/java/testMersysIo/_1_Setup/_1_Parameters/_4_DocumentTypes/DocumentTypes.java
@@ -6,13 +6,10 @@ import io.restassured.http.Cookies;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import testMersysIo._4_Education._2_Login.Login;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
 public class DocumentTypes extends Login {
@@ -32,13 +29,10 @@ public class DocumentTypes extends Login {
                         .contentType(ContentType.JSON)
                         .when()
                         .post(urlLog)
-
                         .then()
                         //.log().all()
                         .statusCode(200)
                         .extract().response().getDetailedCookies();
-        ;
-
         reqSpec = new RequestSpecBuilder()
                 .addCookies(cookies)
                 .setContentType(ContentType.JSON)
@@ -53,66 +47,87 @@ public class DocumentTypes extends Login {
 
         Map<String, String> newDocument = new HashMap<>();
         newDocument.put("name", rndDocumentName);
-        //newDocument.("code", rndDocumentCode);
-        //JsonPath jsonPath = new JsonPath(response);
-        //String selectedValue = jsonPath.get("path.to.desired.field");
-        //newDocument.select("name", "CERTIFICATE");
 
         DocumentID =
                 given()
                         .spec(reqSpec)
-                        .body(newDocument)
+                        .body("{\n" +
+                                "    \"id\":null,\n" +
+                                "    \"name\": \"Team17\",\n" +
+                                "    \"description\": \"\",\n" +
+                                "    \"attachmentStages\": [\n" +
+                                "        \"STUDENT_REGISTRATION\"\n" +
+                                "    ],\n" +
+                                "    \"schoolId\": \"6390f3207a3bcb6a7ac977f9\",\n" +
+                                "    \"active\": true,\n" +
+                                "    \"required\": true,\n" +
+                                "    \"translateName\": [],\n" +
+                                "    \"useCamera\": false\n" +
+                                "}")
+
                         //.log().all()
                         .when()
                         .post(url + "attachments/create")
 
                         .then()
-                        .log().body()
+                        // .log().body()
                         .statusCode(201)
                         .extract().path("id");
-        ;
+        System.out.println("Create Document Type Test: Successfully passed !");
     }
 
     @Test(dependsOnMethods = "createDocumentTypes")
     public void createDocumentNegative() {
-        Map<String, String> newDocument = new HashMap<>();
-        newDocument.put("name", rndDocumentName);
-        //SELECT?
 
         given()
                 .spec(reqSpec)
-                .body(newDocument)
+                .body("{\n" +
+                        "    \"id\":null,\n" +
+                        "    \"name\": \"Team17\",\n" +
+                        "    \"description\": \"\",\n" +
+                        "    \"attachmentStages\": [\n" +
+                        "        \"Certificate\"\n" +
+                        "    ],\n" +
+                        "    \"schoolId\": \"6390f3207a3bcb6a7ac977f9\",\n" +
+                        "    \"active\": true,\n" +
+                        "    \"required\": true,\n" +
+                        "    \"translateName\": [],\n" +
+                        "    \"useCamera\": false\n" +
+                        "}")
 
                 .when()
-                .post(url+"attachments/create")
+                .post(url + "attachments/create")
 
                 .then()
-                .log().body()
+                //.log().body()
                 .statusCode(400)
-                .body("message", containsString("already"))
-        ;
+                .extract().path("id");
+        System.out.println("Create Document Type Negative Test: Successfully passed !");
     }
 
     @Test(dependsOnMethods = "createDocumentNegative")
     public void updateDocument() {
-        String newDocumentName = "Updated Document" + randomUreteci.number().digits(5);
-        Map<String, String> updDocument = new HashMap<>();
-        updDocument.put("id", DocumentID);
-        updDocument.put("name", newDocumentName);
-
-
         given()
                 .spec(reqSpec)
-                .body(updDocument)
-
+                .body("{\n" +
+                        "  \"id\": \"" + DocumentID + "\",\n" +
+                        "    \"name\": \"TechnoTeam17\",\n" +
+                        "    \"description\": \"\",\n" +
+                        "    \"attachmentStages\": [\n" +
+                        "        \"EMPLOYMENT\"\n" +
+                        "    ],\n" +
+                        "    \"schoolId\": \"6390f3207a3bcb6a7ac977f9\",\n" +
+                        "    \"active\": true,\n" +
+                        "    \"required\": true,\n" +
+                        "    \"translateName\": [],\n" +
+                        "    \"useCamera\": false\n" +
+                        "}")
                 .when()
-                .put(url+"attachments")
-
+                .put(url + "attachments")
                 .then()
-                .log().body()
-                .statusCode(200)
-                .body("name", equalTo(newDocumentName))
-        ;
+                // .log().body()
+                .statusCode(200);
+        System.out.println("Update Document Type  Test: Successfully passed !");
     }
 
     @Test(dependsOnMethods = "updateDocument")
@@ -121,11 +136,10 @@ public class DocumentTypes extends Login {
                 .spec(reqSpec)
                 .when()
                 .delete(url + "attachments/" + DocumentID)
-
                 .then()
-                .log().body()
-                .statusCode(200)
-        ;
+                //.log().body()
+                .statusCode(200);
+        System.out.println("Delete Document Type  Test: Successfully passed !");
     }
 
     @Test(dependsOnMethods = "deleteDocument")
@@ -134,12 +148,12 @@ public class DocumentTypes extends Login {
                 .spec(reqSpec)
                 .when()
                 .delete(url + "attachments/" + DocumentID)
-
                 .then()
-                .log().body()
+                //.log().body()
                 .statusCode(400)
-                .body("message", equalTo("Country not found"))
-        ;
-    }
+                .body("message", equalTo("Attachment Type not found"));
+     System.out.println("Delete Document Type Negative Test: Successfully passed !");
+}
+
 
 }
